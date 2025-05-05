a8ee86b9dca3bdfe5d105aea9bc854c57a3f0c4e
goldsteinsalmon <nick@goldsteinsalmon.co.uk>
2025-05-05 08:40:57 +0100

    Create PickersTab.tsx

---
+++ src/components/admin/PickersTab.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PickersTab: React.FC = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Picker Management</h2>
+      <p>Here you can manage the staff who pick orders.</p>
+      {/* TODO: Add picker list and management tools */}
+    </div>
+  );
+};
+
+export default PickersTab;
