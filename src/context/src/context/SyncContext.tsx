477aa915589339c34b81cc005c2f22cca3f13b59
goldsteinsalmon <nick@goldsteinsalmon.co.uk>
2025-05-05 09:20:00 +0100

    Update SyncContext.tsx

--- src/context/src/context/SyncContext.tsx
+++ src/context/src/context/SyncContext.tsx
@@ -1,10 +1,11 @@
 import { createContext, useContext } from "react";
 
+// Create a dummy context — doesn't do anything
 const SyncContext = createContext(null);
 
+// Dummy hook that satisfies the imports but returns nothing
 export const useSyncContext = () => {
   return useContext(SyncContext);
 };
 
 export default SyncContext;
-
