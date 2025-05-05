99f9428fe12208349e597c45703dd901dacdd02f
goldsteinsalmon <nick@goldsteinsalmon.co.uk>
2025-05-05 08:41:25 +0100

    Create SettingsTab.tsx

---
+++ src/components/admin/SettingsTab.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SettingsTab: React.FC = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">System Settings</h2>
+      <p>Configure application-wide settings here.</p>
+      {/* TODO: Add setting controls */}
+    </div>
+  );
+};
+
+export default SettingsTab;
