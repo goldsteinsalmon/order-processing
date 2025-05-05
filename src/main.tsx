e17ac8919a59a1e0fefb8e158c3e1db97878b3cd
goldsteinsalmon <nick@goldsteinsalmon.co.uk>
2025-05-05 09:18:11 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 import { Toaster } from "@/components/ui/toaster";
-import { SyncProvider } from "@/context/SyncContext";
 
 createRoot(document.getElementById("root")!).render(
   <Router>
-    <SyncProvider>
-      <App />
-      <Toaster />
-    </SyncProvider>
+    <App />
+    <Toaster />
   </Router>
 );
